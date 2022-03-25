ea5ebc134c17185143f8ecaca997b134bf453db8
Martyna Król <martyna.maria.krol@gmail.com>
2022-03-25 20:59:00 +0100

    Mentor's suggested corrections applied

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 const buttonRock = document.getElementById('button-rock');
-buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
+buttonRock.addEventListener('click', function(){ 
+  buttonClicked('kamień'); 
+});
 
 const buttonPaper = document.getElementById('button-paper');
-buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
+buttonPaper.addEventListener('click', function(){ 
+  buttonClicked('papier'); 
+});
 
 const buttonScissors = document.getElementById('button-scissors');
-buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
+buttonScissors.addEventListener('click', function(){ 
+  buttonClicked('nożyce'); 
+});
 
 
 
@@ -23,11 +29,11 @@ function buttonClicked(argButtonName) {
 
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
+  if (argMoveId === 1) {
     return 'kamień';
-  } else if (argMoveId == '2') {
+  } else if (argMoveId === '2') {
     return 'papier';
-  } else if (argMoveId == '3') {
+  } else if (argMoveId === '3') {
     return 'nożyce';
   } else {
     printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
@@ -37,13 +43,13 @@ function getMoveName(argMoveId) {
 
 function displayResult(argPlayerMove, argComputerMove) {
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+  if (argPlayerMove === 'papier' && argComputerMove === 'kamień') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+  } else if (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+  } else if (argPlayerMove === 'nożyce' && argComputerMove === 'papier') {
     printMessage('Wygrywasz!');
-  } else if (argPlayerMove == argComputerMove) {
+  } else if (argPlayerMove === argComputerMove) {
     printMessage('Remis!');
   } else {
     printMessage('Przegrywasz :(');
